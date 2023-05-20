e391bbcc3e26f55b8ccdea2780e1dc4bbb12965d
12Ostap12 <115884880+12Ostap12@users.noreply.github.com>
2023-05-20 19:33:19 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pygame import*
-from buttom import Button
+from button import Button
 from sprite import Player
 
 window = display.set_mode((500,700))
@@ -7,14 +7,19 @@ clock = time.Clock()
 
 game = True
 pause = True
+run = False
 
 btn1 = Button(50, 50, 400, 400, 'start.png')
 btn2 = Button(100, 400, 300, 120, 'exit.png')
+car = Player('car.png', 100, 400, 125, 250, 10 )
 bg = image.load('road.jpg')
+
 
 bg = transform.rotate(bg,90)  
 bg = transform.scale(bg, (500, 700))
-
+bg_width = bg.get_height()
+tiles = 3
+scroll = 0
 
 while game:
     for e in event.get():
@@ -25,7 +30,7 @@ while game:
 
             if e.key == K_ESCAPE:
                 pause = True
-    
+
     if pause == True:
         window.fill((255, 255, 255))
         if btn1.draw(window):
@@ -33,6 +38,16 @@ while game:
         if btn2.draw(window):
             game = False          
     else:
-        window.blit(bg, (0, 0)) 
+   
+        for i in range(0, tiles):
+            window.blit(bg, (0,i * bg_width + scroll))
+        scroll += 3
+
+        if scroll > 700:
+            scroll = 0
+
+        car.draw(window)
+        car.move()
+
     display.update()
     clock.tick(60)
--- sprite.py
+++ sprite.py
@@ -4,6 +4,7 @@ class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
         super().__init__()
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.image = transform.rotate(self.image, 180)
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
